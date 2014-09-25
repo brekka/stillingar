c7a08fbe14be03bd876f541baafb0787e152b868
Andrew Taylor <andrew@brekka.org>
2014-09-25 23:01:22 +0000

    Inital branch for adding Previous annotation

---
+++ api/src/main/java/org/brekka/stillingar/api/annotations/Previous.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.brekka.stillingar.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Can be applied to a parameter of a {@link ConfigurationListener} method to indicate that the parameter should be
+ * called with the previous value in the event of an update, or null if this is the first invocation.
+ *
+ * @author Andrew Taylor
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.PARAMETER })
+public @interface Previous {
+
+}
