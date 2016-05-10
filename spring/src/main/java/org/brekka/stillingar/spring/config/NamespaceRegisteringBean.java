a6e47f34421a1ed4d674a1149e88098bb55fbf41
Andrew Taylor <andrew.taylor@digitalshadows.com>
2016-05-10 19:59:54 +0000

    Second attempt at fixing the namespace handling when loading replacement configuration service contexts.

--- spring/src/main/java/org/brekka/stillingar/spring/config/NamespaceRegisteringBean.java
+++ spring/src/main/java/org/brekka/stillingar/spring/config/NamespaceRegisteringBean.java
@@ -16,7 +16,7 @@
 
 package org.brekka.stillingar.spring.config;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -24,88 +24,35 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.brekka.stillingar.core.dom.NamespaceAware;
-import org.brekka.stillingar.spring.bpp.ConfigurationBeanPostProcessor;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.core.Ordered;
 
 /**
- * A simple bean that registers the specified namespace uri and prefix on load. At this time it is abusing the {@link BeanPostProcessor}
- * mechanism in order to run before {@link ConfigurationBeanPostProcessor}.
+ * A simple bean that registers the specified namespace uri and prefix with the NamespaceAware.
  *
  * @author Andrew Taylor (andrew@brekka.org)
  */
-public class NamespaceRegisteringBean implements BeanPostProcessor, Ordered {
+public class NamespaceRegisteringBean {
 
     private static final Log log = LogFactory.getLog(NamespaceRegisteringBean.class);
-    
+
     private final NamespaceAware namespaceAware;
     private Map<String, String> namespaceToPrefixMap;
-    
-    /**
-     * @param loader
-     * @param uri
-     * @param prefix
-     */
-    public NamespaceRegisteringBean(NamespaceAware namespaceAware, String uri, String prefix) {
-        this(namespaceAware, asMap(uri, prefix));
+
+    public NamespaceRegisteringBean(final NamespaceAware namespaceAware, final String uri, final String prefix) {
+        this(namespaceAware, Collections.singletonMap(uri, prefix));
     }
 
-    /**
-     * @param loader
-     * @param uri
-     * @param prefix
-     */
-    public NamespaceRegisteringBean(NamespaceAware namespaceAware, Map<String, String> namespaceToPrefixMap) {
+    public NamespaceRegisteringBean(final NamespaceAware namespaceAware, final Map<String, String> namespaceToPrefixMap) {
         this.namespaceAware = namespaceAware;
-        this.namespaceToPrefixMap = namespaceToPrefixMap;
+        this.namespaceToPrefixMap = namespaceToPrefixMap == null ? Collections.<String, String>emptyMap() : namespaceToPrefixMap;
     }
-    
 
-    /* (non-Javadoc)
-     * @see org.springframework.beans.factory.config.BeanPostProcessor#postProcessBeforeInitialization(java.lang.Object, java.lang.String)
-     */
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        if (namespaceToPrefixMap != null) {
-            Set<Entry<String,String>> entrySet = namespaceToPrefixMap.entrySet();
-            for (Entry<String, String> entry : entrySet) {
-                if (log.isInfoEnabled()) {
-                    log.info(String.format("Registering prefix '%s' to namespace '%s'", entry.getValue(), entry.getKey()));
-                }
-                namespaceAware.registerNamespace(entry.getValue(), entry.getKey());
+    public void registerNamespaces() {
+        Set<Entry<String,String>> entrySet = namespaceToPrefixMap.entrySet();
+        for (Entry<String, String> entry : entrySet) {
+            if (log.isInfoEnabled()) {
+                log.info(String.format("Registering prefix '%s' to namespace '%s'", entry.getValue(), entry.getKey()));
             }
-            namespaceToPrefixMap = null;
+            namespaceAware.registerNamespace(entry.getValue(), entry.getKey());
         }
-        return bean;
     }
-
-    /* (non-Javadoc)
-     * @see org.springframework.beans.factory.config.BeanPostProcessor#postProcessAfterInitialization(java.lang.Object, java.lang.String)
-     */
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return bean;
-    }
-    
-    /* (non-Javadoc)
-     * @see org.springframework.core.Ordered#getOrder()
-     */
-    @Override
-    public int getOrder() {
-        // Need to run before ConfigurationBeanPostProcessor
-        return 9;
-    }
-    
-    /**
-     * @param uri
-     * @param prefix
-     * @return
-     */
-    private static Map<String, String> asMap(String uri, String prefix) {
-        Map<String, String> map = new HashMap<String, String>();
-        map.put(uri, prefix);
-        return map;
-    }
-
 }
