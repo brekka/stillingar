94d982e5bf4aafd83317bae162a80ef6e1721bbc
Andrew Taylor <andrew.taylor@digitalshadows.com>
2016-05-10 20:26:18 +0100

    Revert "Change mechanism for registering namespaces so that it happens after bean definition. This allows namespaces to be collected correctly even when bean definitions are overridden"
    This reverts commit 53d3a05ca568abb82d57e7713c65f5b6b37649f6.

--- spring/src/main/java/org/brekka/stillingar/spring/config/NamespaceBeanDefinitionParser.java
+++ spring/src/main/java/org/brekka/stillingar/spring/config/NamespaceBeanDefinitionParser.java
@@ -16,19 +16,29 @@
 
 package org.brekka.stillingar.spring.config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConstructorArgumentValues;
+import org.springframework.beans.factory.config.ConstructorArgumentValues.ValueHolder;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.ManagedArray;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 /**
- * Map the 'namespace' element to a {@link NamespaceRegisteringBean} that can be picked up by
- * {@link DefaultNamespaceContextFactoryBean} and/or registered itself via {@link NamespaceRegisteringBean}.
+ * External namespace configurer.
  *
  * @author Andrew Taylor (andrew@brekka.org)
  */
 class NamespaceBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+    private static final Log log = LogFactory.getLog(NamespaceBeanDefinitionParser.class);
 
+    /* (non-Javadoc)
+     * @see org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser#doParse(org.w3c.dom.Element, org.springframework.beans.factory.xml.ParserContext, org.springframework.beans.factory.support.BeanDefinitionBuilder)
+     */
     @Override
     protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
         String prefix = element.getAttribute("prefix");
@@ -37,19 +47,39 @@ class NamespaceBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
             // Older versions had the attribute incorrectly named 'url'.
             uri = element.getAttribute("url");
         }
-        String serviceId = element.getAttribute("service-ref");
-        builder.addConstructorArgValue(serviceId);
-        builder.addConstructorArgValue(prefix);
+        String serviceRef = element.getAttribute("service-ref");
+        String namespaceContextId = serviceRef + "-Namespaces";
+        try {
+            BeanDefinition namespaceBeanDefinition = parserContext.getRegistry().getBeanDefinition(namespaceContextId);
+            // In the same context, make sure to register the namespace as early as possible.
+            ConstructorArgumentValues constructorArgumentValues = namespaceBeanDefinition.getConstructorArgumentValues();
+            ValueHolder valueHolder = constructorArgumentValues.getIndexedArgumentValue(0, null);
+            Object value = valueHolder.getValue();
+            ManagedArray array = (ManagedArray) value;
+            array.add(prefix);
+            array.add(uri);
+        } catch (NoSuchBeanDefinitionException e) {
+            if (log.isInfoEnabled()) {
+                log.info(String.format("No namespaces context found with id '%s' in the current container", namespaceContextId), e);
+            }
+        }
+        builder.addConstructorArgReference(serviceRef + "-Namespaces");
         builder.addConstructorArgValue(uri);
+        builder.addConstructorArgValue(prefix);
     }
-
+    
     @Override
     protected boolean shouldGenerateId() {
         return true;
     }
-
+    
+    /* (non-Javadoc)
+     * @see org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser#getBeanClass(org.w3c.dom.Element)
+     */
     @Override
     protected Class<?> getBeanClass(Element element) {
         return NamespaceRegisteringBean.class;
     }
+    
+    
 }
